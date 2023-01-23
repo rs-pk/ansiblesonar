614d84cd877998fb4b0b3b42cab995a589b28098
satgurudas <47414473+satgurudas@users.noreply.github.com>
2023-01-23 17:18:26 +0530

    updated netstat command

--- setup.sh
+++ setup.sh
@@ -2,5 +2,6 @@
 		sudo apt update -y
 		sudo apt install ansible -y
 		sudo apt-get install zip unzip
+		sudo apt install net-tools
 		sudo git clone https://github.com/rs-pk/ansiblesonar.git
 		sudo ansible-playbook ansiblesonar/ansible_config/site.yml
