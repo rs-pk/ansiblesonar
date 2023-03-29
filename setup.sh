8eea743452e1cf35a71b30b43987d48f099cd0bd
satgurudas <47414473+satgurudas@users.noreply.github.com>
2023-03-29 15:41:49 +0530

    ansible role for jfrog

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 		sudo apt install ansible -y
 		sudo apt-get install zip unzip
 		sudo apt install net-tools
-		sudo ansible-galaxy collection install jfrog.ansible
+		# sudo ansible-galaxy collection install jfrog.ansible
 		sudo git clone https://github.com/rs-pk/ansiblesonar.git
-		# sudo ansible-playbook ansiblesonar/ansible_config/site.yml
-		sudo ansible-playbook -i ansiblesonar/ansible_config/inventory.yml ansiblesonar/ansible_config/playbook.yml
+		sudo ansible-playbook ansiblesonar/ansible_config/site.yml
+		# sudo ansible-playbook -i ansiblesonar/ansible_config/inventory.yml ansiblesonar/ansible_config/playbook.yml
