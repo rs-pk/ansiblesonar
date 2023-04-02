917067c98c9d15906e31b995543bd50fab54a99a
satgurudas <47414473+satgurudas@users.noreply.github.com>
2023-04-02 17:36:32 +0530

    updated

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-		#sudo apt-add-repository ppa:ansible/ansible -y
+		sudo apt-add-repository ppa:ansible/ansible -y
 		sudo apt update -y
 		sudo apt install ansible -y
 		sudo apt-get install zip unzip
