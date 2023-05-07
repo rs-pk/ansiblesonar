dd704d1cc4770edd0002522a509f6dad31d4de6a
satgurudas <47414473+satgurudas@users.noreply.github.com>
2023-05-07 17:54:41 +0530

    update

--- setup.sh
+++ setup.sh
@@ -1,24 +1,5 @@
-		# sudo apt-add-repository ppa:ansible/ansible -y
-		# sudo apt update -y
-		# sudo apt install ansible -y
-		# sudo apt-get install zip unzip
-		# sudo apt install net-tools
-		# sudo ansible-galaxy collection install jfrog.ansible
-		# sudo git clone https://github.com/rs-pk/ansiblesonar.git
-		# sudo ansible-playbook ansiblesonar/ansible_config/site.yml
-		# sudo ansible-playbook -i ansiblesonar/ansible_config/inventory.yml ansiblesonar/ansible_config/playbook.yml
+#!/bin/bash
 
-
-
-
-
-
-# mkfs.ext4 /dev/sdc
-# mkdir -p /data
-
-# mount -t ext4 /dev/sdc /data
-
-# timedatectl set-timezone Asia/Calcutta
-
-sudo apt update -y
-# sudo apt upgarde -y
+echo "Updating system..."
+sudo apt-get update
+sudo apt-get upgrade
