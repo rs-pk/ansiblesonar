7ef1d57694abb73caf8e540bc98e9da46f94f602
satgurudas <47414473+satgurudas@users.noreply.github.com>
2023-05-07 17:13:59 +0530

    update

--- setup.sh
+++ setup.sh
@@ -13,11 +13,12 @@
 
 
 
-mkfs.ext4 /dev/sdc
-mkdir -p /data
+# mkfs.ext4 /dev/sdc
+# mkdir -p /data
 
-mount -t ext4 /dev/sdc /data
+# mount -t ext4 /dev/sdc /data
 
-timedatectl set-timezone Asia/Calcutta
-apt update -y
-apt upgarde -y
+# timedatectl set-timezone Asia/Calcutta
+
+sudo apt update -y
+sudo apt upgarde -y
